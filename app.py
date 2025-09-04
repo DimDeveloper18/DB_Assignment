b4591856f6e52a6c77cd04f2f8e2f7cb0d1f8466
DimDeveloper18 <dimash.itcout@gmail.com>
2025-09-04 19:40:19 +0100

    reg_page,log_page added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from statistics import median, mean
-from flask import Flask, render_template, request, url_for, redirect
-from flask_login_form import (
+from flask import Flask, flash, render_template, request, url_for, redirect
+from flask_login import (
     LoginManager,
     login_user,
     logout_user,
@@ -9,12 +9,14 @@ from flask_login_form import (
 )
 
 from models import Customer, dbase
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app = config.from_object('config')
+app.config.from_object('config')
 
 login_manager = LoginManager(app)
-login_manager.login_view = "index"
+login_manager.login_view = "log_page"
 
 with app.app_context():
     dbase.init_app(app)
@@ -24,46 +26,58 @@ def load_user(user_id):
     return Customer.query.get(int(user_id))
 
 @app.route("/")
-
-@app.route("/index", methods=["GET"])
+@app.route("/index")
 def index():
     return render_template("index.html")
 
-@app.route("/index", methods=["POST"])
+@app.route("/reg_page", methods=["GET"])
+def register_page():
+    return render_template("reg_page.html")
+
+@app.route("/reg_page", methods=["POST"])
 def registr_form():
-    username = request.form["username"]
+    username = request.form["email"]
     password = request.form["password"]
-    if User.query.filter_by(username=username).first():
+    if Customer.query.filter_by(username=username).first():
         flash(f"The username already exist.")
-        return redirect(url_for("index"))
+        return redirect(url_for("register_page"))
     
-    user = User(username=username, password=password)
+    user = Customer(username=username, password=password)
     dbase.session.add(user)
     login_user(user)
     flash(f"Welcome {username}!")
     return redirect(url_for("index"))
 
-@app.route("/index", methods=["POST"])
+@app.route("/log_page", methods=["GET"])
+def login_page():
+    return render_template("log_page.html")
+
+@app.route("/log_page", methods=["POST"])
 def login_form():
-    username = request.form["username"]
+    username = request.form["email"]
     password = request.form["password"]
     user =  Customer.query.filter_by(username=username).first()
     if not user:
         flash(f"Incorrect username. Try again!")
-        return redirect(url_for("index"))
+        return redirect(url_for("login_page"))
     if password != user.password:
         flash(f"Invalid password. Try again!")
-        return redirect(url_for("index"))
+        return redirect(url_for("login_page"))
 
     login_user(user)
     flash(f"Welcome back, {username}!")
     return redirect(url_for("index"))
 
-@app.route("/index", methods=["POST"])
+@app.route("/logout_page", methods=["GET"])
+@login_required
+def logout():
+    return render_template("logout_page.html")
+
+@app.route("/logout_page", methods=["POST"])
 @login_required
 def logout_form():
     logout_user()
-    flash("Log out successfull")
+    flash("Log out successfull.")
     return redirect(url_for("index"))
 
 @app.route("/tools.html")
--- static/js/registr.js
+++ static/js/registr.js
@@ -1,83 +1,83 @@
-                            // Home page code
-                            // Registration button code
+//                             // Home page code
+//                             // Registration button code
 
-const floatRegForm = document.getElementById("regForm");
-const closeWinReg = document.getElementById("closeBtnReg");
-const btnReg = document.getElementById("regBtn");
-btnReg.addEventListener("click", () => {
-    floatRegForm.style.display = "block";
-});
-closeWinReg.addEventListener("click", () => {
-    floatRegForm.style.display = "none";
-});
+// const floatRegForm = document.getElementById("regForm");
+// const closeWinReg = document.getElementById("closeBtnReg");
+// const btnReg = document.getElementById("regBtn");
+// btnReg.addEventListener("click", () => {
+//     floatRegForm.style.display = "block";
+// });
+// closeWinReg.addEventListener("click", () => {
+//     floatRegForm.style.display = "none";
+// });
 
-                              // Login button code
+//                               // Login button code
 
-const btnLog = document.getElementById("logBtn");
-const floatLogForm = document.getElementById ("logForm")
-const closeWinLog = document.getElementById("closeBtnLog");
-btnLog.addEventListener("click", () => {
-    floatLogForm.style.display = "block";
-});
-closeWinLog.addEventListener("click", () => {
-    floatLogForm.style.display = "none";
-});
+// const btnLog = document.getElementById("logBtn");
+// const floatLogForm = document.getElementById ("logForm")
+// const closeWinLog = document.getElementById("closeBtnLog");
+// btnLog.addEventListener("click", () => {
+//     floatLogForm.style.display = "block";
+// });
+// closeWinLog.addEventListener("click", () => {
+//     floatLogForm.style.display = "none";
+// });
 
-                                // Submit button code for Register form
+//                                 // Submit button code for Register form
 
-const btnSub = document.getElementById("subRegBtn");
-btnSub.addEventListener("click", () => {
-    floatRegForm.style.display = "block";
-});
+// const btnSub = document.getElementById("subRegBtn");
+// btnSub.addEventListener("click", () => {
+//     floatRegForm.style.display = "block";
+// });
 
-floatRegForm.addEventListener("submit", function(event){
-    event.preventDefault();
-    createStatusCell.innerText = "";
+// floatRegForm.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     createStatusCell.innerText = "";
     
-    const mail = document.getElementById("emailReg").value;
-    const pass = document.getElementById("passwordReg").value;
-    if (!mail || !pass){
-        createStatusCell.innerText = '<p class= "error">" All fealds are required. </p>';
-    }else if (pass.length < 6){
-        createStatusCell.innerText = '<p class= "error"> Password must be at least 6 charachters long. </p>';
-    }else {
-        '<p class= "error">" You are regestered! </p>';
-    }
-});
+//     const mail = document.getElementById("emailReg").value;
+//     const pass = document.getElementById("passwordReg").value;
+//     if (!mail || !pass){
+//         createStatusCell.innerText = '<p class= "error">" All fealds are required. </p>';
+//     }else if (pass.length < 6){
+//         createStatusCell.innerText = '<p class= "error"> Password must be at least 6 charachters long. </p>';
+//     }else {
+//         '<p class= "error">" You are regestered! </p>';
+//     }
+// });
 
-const btnSub2 = document.getElementById("subLogBtn");
-btnSub.addEventListener("click", () => {
-    floatLogForm.style.display = "block";
-});
-                                // Submit button code for Login form
+// const btnSub2 = document.getElementById("subLogBtn");
+// btnSub.addEventListener("click", () => {
+//     floatLogForm.style.display = "block";
+// });
+//                                 // Submit button code for Login form
 
-floatLogForm.addEventListener("submit", function(event){
-    event.preventDefault();
-    const statusCell = document.getElementById("emailErrorStatus");
-    const mail = document.getElementById("emailLog").value;
-    const pass = document.getElementById("passwordLog").value;
-console.log("mail:" +mail);
-console.log("pass:" +pass);
-console.log("!mail" +!mail);
-console.log("!pass" +!pass);
-    if (!mail || !pass){
-        console.log("inside !mail || !pass");
-        statusCell.innerHTML = '<p class= "error"> All fields are required. </p>';
-    }else if (pass.length < 6){
-        statusCell.innerHTML = '<p class= "error"> Password must be at least 6 charachters long. </p>';
-    }else {
-        statusCell.innerHTML = '<p class= "error"> You are regestered! </p>';
-    }
-});
+// floatLogForm.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     const statusCell = document.getElementById("emailErrorStatus");
+//     const mail = document.getElementById("emailLog").value;
+//     const pass = document.getElementById("passwordLog").value;
+// console.log("mail:" +mail);
+// console.log("pass:" +pass);
+// console.log("!mail" +!mail);
+// console.log("!pass" +!pass);
+//     if (!mail || !pass){
+//         console.log("inside !mail || !pass");
+//         statusCell.innerHTML = '<p class= "error"> All fields are required. </p>';
+//     }else if (pass.length < 6){
+//         statusCell.innerHTML = '<p class= "error"> Password must be at least 6 charachters long. </p>';
+//     }else {
+//         statusCell.innerHTML = '<p class= "error"> You are regestered! </p>';
+//     }
+// });
 
-                                // Basket button cod
+//                                 // Basket button cod
                                 
-const btnBask = document.getElementById("bask");
-const closeBaskWind = document.getElementById("closeBaskPrev");
-const floatBaskWind = document.getElementById("baskPrev");
-btnBask.addEventListener("click", () => {
-    floatBaskWind.style.display = "block";
-});
-closeBaskWind.addEventListener("click", () => {
-    floatBaskWind.style.display = "none";
-});
+// const btnBask = document.getElementById("bask");
+// const closeBaskWind = document.getElementById("closeBaskPrev");
+// const floatBaskWind = document.getElementById("baskPrev");
+// btnBask.addEventListener("click", () => {
+//     floatBaskWind.style.display = "block";
+// });
+// closeBaskWind.addEventListener("click", () => {
+//     floatBaskWind.style.display = "none";
+// });
