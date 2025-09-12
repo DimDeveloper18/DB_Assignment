e5a6f60d400f8c1f3ec5b530d2f9d23b7d0eb6ce
DimDeveloper18 <dimash.itcout@gmail.com>
2025-09-12 19:36:56 +0100

    File 'models.py' code changed: class 'CustomerComment' created for comments table and content (id, msgname, comment, writer_id), function 'str' added.

--- models.py
+++ models.py
@@ -11,3 +11,14 @@ class Customer(UserMixin, dbase.Model):
 
     def __str__(self):
         return self.username
+    
+class CustomerComment(dbase.Model):
+    id = dbase.mapped_column(dbase.Integer, primary_key=True)
+    msgname = dbase.mapped_column(dbase.String(80), nullable=False)
+    comment = dbase.mapped_column(dbase.Text, nullable=False)
+    writer_id = dbase.mapped_column(dbase.Integer, dbase.ForeignKey('customer.id'), nullable=False)
+
+    writer = dbase.relationship('Customer', backref=dbase.backref('customer_comments', lazy=True))
+
+    def __str__(self):
+        return f'"{self.msgname}" by {self.writer.username}'
