b1d1ac96dfbe4fb15ace4310f6fc0a5e2f0cc4fd
DimDeveloper18 <dimash.itcout@gmail.com>
2025-09-14 19:23:12 +0100

    File 'config.py' code changed: database file name changed.

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 import os
 SECRET_KEY=os.getenv('SECRET_KEY', 'not-set')
-SQLALCHEMY_DATABASE_URI=os.getenv('LOCAL_DATABASE_URL', 'sqlite:///db.sqlite3')
+SQLALCHEMY_DATABASE_URI=os.getenv('LOCAL_DATABASE_URL', 'sqlite:///database.db')
